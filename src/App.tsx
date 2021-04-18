2c8687fd7b7933887ebf1fbe191b411a4661debc
Pablo Tapia <ptapia@proyecto404.com>
2021-04-18 01:52:59 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const {useState} = React;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let cardsColors = ['pink', 'blue', 'yellow', 'green']
+    let rockets = [1, 2, 3, 4]
+    const Card = (cardValue: number, type: string) => {
+        const [isSelected, select] = useState(false)
+        const classes = `${type} ${isSelected ? 'selected' : ''} `
+        return <div className={classes} style={styles.card}
+                    onClick={() => {
+                        select(!isSelected);
+                    }}> {cardValue} </div>
+    }
+    return (
+        <div className="App">
+            <div style={styles.container}>
+                <h2 style={{color: 'white'}}>The Crew</h2>
+                <div style={styles.section}>
+                    {rockets.map(it => {
+                        return Card(it, 'rocket')
+                    })}
+                </div>
+                {cardsColors.map(color => {
+                    return <div style={styles.section}>
+                        {cards.map(it => {
+                            return Card(it, color)
+                        })}
+                    </div>
+                })}
+            </div>
+        </div>
+    );
 }
 
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        backgroundColor: '#282c34',
+    } as React.CSSProperties,
+    card: {
+        margin: 10,
+        padding: 20,
+        borderRadius: 10,
+        width: '25',
+        height: '50',
+    } as React.CSSProperties,
+    section: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    } as React.CSSProperties
+
+}
 export default App;
